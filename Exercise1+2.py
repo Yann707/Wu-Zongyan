99a0651b54bc13589c2b95d09813af188c226e93
Yann707 <zongyanw@metropolia.fi>
2024-08-22 10:18:02 +0300

    Wu Zongyan

--- Exercise1+2.py
+++ Exercise1+2.py
@@ -19,4 +19,16 @@ print(f"The area of the circle is: {area:}")
 
 #3
 
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+perimeter = (length + width) * 2
+
+area = length * width
+
+print(f"The perimeter of the rectangle is: {perimeter:}")
+print(f"The area of the rectangle is: {area:}")
+
+#4
+
 
