4221c51fdc13b4d1f10b32205ea19359afb435de
Yann707 <zongyanw@metropolia.fi>
2024-08-22 09:50:11 +0300

    Wu Zongyan

--- Exercise1+2.py
+++ Exercise1+2.py
@@ -1,3 +1,22 @@
 #Exercise1
 print("Hello, Wu Zongyan!")
 
+#Exercise2
+#1
+name = input("Enter your name here: ")
+
+print(f"Hello, {name}!")
+
+#2
+
+import math
+
+radius = float(input("Enter the radius of the circle: "))
+
+area = math.pi * radius ** 2
+
+print(f"The area of the circle is: {area:}")
+
+#3
+
+
