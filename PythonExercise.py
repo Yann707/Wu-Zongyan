4d36110b948fa1835cc8708f4d94b6e6f54d8720
Yann707 <zongyanw@metropolia.fi>
2024-08-26 23:58:05 +0300

    Wu Zongyan

--- PythonExercise.py
+++ PythonExercise.py
@@ -89,13 +89,13 @@ print("4-digit code:", code4)'''
 
 #Exercise2
 
-size_limit = 42
+size = 42
 zander_length = float(input("Enter the length of the zander in centimeters: "))
 
-if zander_length >= size_limit:
+if zander_length >= size:
     print("No problem! You can keep the zander.")
 else:
-    centimeters_short = size_limit - zander_length
+    centimeters_short = size - zander_length
     print(f"The zander is {centimeters_short:.2f} centimeters below the size limit.")
     print("Please release the fish back into the lake.")
 
