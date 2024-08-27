5847a0e6634b33d9d79883c2022a1e5e34d0105e
Yann707 <zongyanw@metropolia.fi>
2024-08-27 17:06:06 +0300

    Wu Zongyan

--- VariableDemo.py
+++ VariableDemo.py
@@ -1,4 +1,4 @@
-'''a = int(input("Enter the number a: "))
+a = int(input("Enter the number a: "))
 b = int(input("Enter the number b: "))
 
 a, b = b, a
@@ -14,7 +14,7 @@ perimeter = (length + width) * 2
 area = length * width
 
 print(f"The perimeter of the rectangle is: {perimeter:}")
-print(f"The area of the rectangle is: {area:}")'''
+print(f"The area of the rectangle is: {area:}")
 
 age = int(input("Enter your age: "))
 if 15 <= age < 18:
