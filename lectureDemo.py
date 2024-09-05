cb472fcd866b8a0192aa6f139cee9f0f07c67650
Yann707 <zongyanw@metropolia.fi>
2024-09-05 11:15:45 +0300

    Wu Zongyan

--- lectureDemo.py
+++ lectureDemo.py
@@ -14,7 +14,7 @@ user_input= ""
 while user_input != "exit":
     user_input = input("Type something (or exit to quit): ")
     print("You typed:", user_input)
-'''
+
 number = []
 
 while True:
@@ -33,3 +33,29 @@ if number:
 else:
     print("No numbers were entered.")
 
+names = []
+
+name = input("Enter the first name or quit by pressing Enter: ")
+while name != "":
+    names.append(name)
+    name = input("Enter the next name or quit by pressing Enter: ")
+
+print("Names list:", names)
+
+otherNames = ["Allu", "Ninni"]
+names.extend(otherNames)
+print("After extending with ['Allu', 'Ninni']:", names)
+'''
+
+def num(a, b):
+    total = a + b
+    print(f"The sum is: {total}")
+    subtraction = a - b
+    print(f"The subtraction is: {subtraction}")
+    multiplication = a * b
+    print(f"The multiplication is: {multiplication}")
+    division = a / b
+    print(f"The division is: {division}")
+    return total, subtraction, multiplication, division
+
+num(50,4)
